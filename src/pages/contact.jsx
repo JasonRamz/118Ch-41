a70fd3535389b77139661624e0fe30daa3437885
Jason Ramirez <12ramirezjason@gmail.com>
2024-02-08 22:50:05 -0800

    waiting for comfirmation to finish contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,29 +3,37 @@ import './contact.css';
 
 function Contact() {
     return (
-        <div className="contact page">
+        <div className="contact-page page">
+            <div className="contact-description">
                 <h1>Contact Us</h1>
-                <br />
-                <h2>Email Address</h2>
-
-                <p>You can reach us via email at:</p>
-                <br />
-                <h2>Contact Form</h2>
-                <form>
-                    <label>Name:</label>
-                    <input name="name" required />
-                    <br />
-                    <br/>
-                    <label>Email:</label>
-                    <input name="email" required />
-                    <br />
-                    <br/>
-                    <label>Message:</label><br />
-                    <textarea className='message' rows="4" required></textarea><br />
+                <p>Thank you for reaching out to LashRoom. We value your interest and are here to assist you in any way we can.</p>
+                <p>Please feel free to use the form below to send us your inquiries, comments, or feedback. We aim to respond to all messages promptly.</p>
+            </div>
+            <div className="contact-form">
+                <h2>Get In Touch</h2>
+                <form className='form-container'>
+                    <div>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" id="name" name="name" placeholder="Full Name" required />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" placeholder="your-email@example.com" required />
+                    </div>
+                    <div>
+                        <label htmlFor="phone">Phone:</label>
+                        <input type="tel" id="phone" name="phone" placeholder="Enter your Phone" />
+                    </div>
+                    <div>
+                        <label htmlFor="message">Message:</label><br />
+                        <textarea id="message" name="message" placeholder="Description..." rows="4" required></textarea><br />
+                    </div>
                     <button type="submit">Submit</button>
                 </form>
+            </div>
         </div>
     );
 }
 
 export default Contact;
+
